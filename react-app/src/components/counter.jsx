f2c9fb13a2b894548bbf8412e893b27c3b1951c0
khantech166 <khan.tech885@gamil.com>
2020-09-21 15:28:32 +0500

    working with counter

---
+++ react-app/src/components/counter.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  render() {
+    return (
+      <div>
+        <span style={{ fontSize: 10 }} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondery btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li>{tag}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+export default Counter;
